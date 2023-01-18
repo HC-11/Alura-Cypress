d5d1958691ca20f8f23ff934d71fe26335c27632
HC <53163224+HC-11@users.noreply.github.com>
2023-01-18 16:24:19 -0300

    Aula 4 - Utilizando massa de dados
    Como fazer um teste de login com varios usuarios de uma lista em .json

---
+++ alura-pic-novoUsuario.cy.js
@@ -0,0 +1,19 @@
+describe('Acessando AluraPic', () => {
+   beforeEach(()=> {
+      cy.visit('https://alura-fotos.herokuapp.com');
+   })
+
+   const usuarios = require('../../fixtures/usuarios.json')
+   usuarios.forEach(usuario => {
+     it.only(`Registrar novo usuario' ${usuario.userName}`, () => {          
+      cy.contains('a', 'Register now').click();
+      cy.contains('button', 'Register').click();
+      cy.get('input[formcontrolname="email"]').type(usuario.email);
+      cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+      cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+      cy.get('input[formcontrolname="password"]').type(usuario.password);
+      cy.contains('button', 'Register').click();
+     }) 
+ })
+   
+})
